7f4a9ea1b29d86bca947b9ea2e21f8a7521a84d5
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2025-02-28 22:51:33 -0300

    primeiro commit

---
+++ src/main/java/com/LSsoftware/ProjetoDecolaTec/ProjetoDecolaTecApplication.java
@@ -0,0 +1,13 @@
+package com.LSsoftware.ProjetoDecolaTec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDecolaTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDecolaTecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LSsoftware/ProjetoDecolaTec/ProjetoDecolaTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LSsoftware.ProjetoDecolaTec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDecolaTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
